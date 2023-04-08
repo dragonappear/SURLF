f71bde5187b89b74f4e678bf45a44572c29da9db
dragonappear <dragonappear1@gmail.com>
2023-04-09 04:12:17 +0900

    Feat: add QueryHint using index

--- java/src/main/java/me/dragonappear/domain/link/ShortLinkRepository.java
+++ java/src/main/java/me/dragonappear/domain/link/ShortLinkRepository.java
@@ -1,6 +1,8 @@
 package me.dragonappear.domain.link;
 
+import jakarta.persistence.QueryHint;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -14,7 +16,9 @@ public interface ShortLinkRepository extends JpaRepository<ShortLinkEntity, Long
 
     Optional<ShortLinkEntity> findByOriginalUrl(String url);
 
+    @QueryHints(value = @QueryHint(name = "org.hibernate.annotations.QueryHints.USE_INDEXES", value = "uk_short_id"))
     Optional<ShortLinkEntity> findByShortId(String shortId);
 
+    @QueryHints(value = @QueryHint(name = "org.hibernate.annotations.QueryHints.USE_INDEXES", value = "uk_short_id"))
     boolean existsByShortId(String shortId);
 }
