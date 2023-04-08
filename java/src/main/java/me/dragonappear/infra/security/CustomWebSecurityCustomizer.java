82bab02f7c0095f4ab616e0c648b1fb46ac89871
dragonappear <dragonappear1@gmail.com>
2023-04-09 01:32:42 +0900

    Feat: add spring-security module
    disable below apis
    - formLogin
    - httpBasic
    - csrf

---
+++ java/src/main/java/me/dragonappear/infra/security/CustomWebSecurityCustomizer.java
@@ -0,0 +1,50 @@
+package me.dragonappear.infra.security;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.annotation.web.configurers.SessionManagementConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class CustomWebSecurityCustomizer implements WebSecurityCustomizer {
+
+    @Override
+    public void customize(WebSecurity web) {
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin().disable()
+                .httpBasic().disable()
+                .logout().disable()
+                .rememberMe().disable()
+                .csrf().disable()
+                .sessionManagement(getSessionManagementConfigurer());
+
+        return http.build();
+    }
+
+    private Customizer<SessionManagementConfigurer<HttpSecurity>> getSessionManagementConfigurer() {
+        return (SessionManagementConfigurer) -> SessionManagementConfigurer
+                .sessionCreationPolicy(SessionCreationPolicy.ALWAYS)
+                .sessionFixation().changeSessionId();
+    }
+
+}
