582ecd415c193f8100d82993650704fae3baf3a4
dragonappear <dragonappear1@gmail.com>
2023-04-10 07:49:36 +0900

    Feat: add Active profile on KafkaEvent

--- java/src/main/java/me/dragonappear/domain/link/event/ShortLinkKafkaEventListener.java
+++ java/src/main/java/me/dragonappear/domain/link/event/ShortLinkKafkaEventListener.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.scheduling.annotation.Async;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Async
 @Component
+@Profile(value = {"local", "default"})
 @RequiredArgsConstructor
 public class ShortLinkKafkaEventListener {
 
